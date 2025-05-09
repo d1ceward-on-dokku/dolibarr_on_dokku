3b61d6a3770222e1b90a4a8528b9daa17f818b41
Fabien LEFEBVRE (d1ceward) <contact@d1ceward.com>
2025-05-09 23:42:02 +0200

    Update to official docker image

--- Dockerfile
+++ Dockerfile
@@ -1,59 +1,7 @@
-FROM php:8.2-apache-bookworm
+ARG DOLIBARR_VERSION="20.0.3"
 
-ENV DOLIBARR_VERSION="20.0.3"
-ENV DOLIBARR_INSTALL_AUTO 1
-ENV DOLIBARR_PROD 1
+FROM dolibarr/dolibarr:${DOLIBARR_VERSION}
 
-ENV DOLIBARR_URL_ROOT 'http://localhost'
+COPY entrypoint.sh /custom-entrypoint.sh
 
-ENV WWW_USER_ID 33
-ENV WWW_GROUP_ID 33
-
-ENV PHP_INI_DATE_TIMEZONE 'UTC'
-ENV PHP_INI_MEMORY_LIMIT 256M
-ENV PHP_INI_UPLOAD_MAX_FILESIZE 2M
-ENV PHP_INI_POST_MAX_SIZE 8M
-ENV PHP_INI_ALLOW_URL_FOPEN 0
-
-RUN apt-get update -y \
-    && apt-get dist-upgrade -y \
-    && apt-get install -y --no-install-recommends \
-        libc-client-dev \
-        libfreetype6-dev \
-        libjpeg62-turbo-dev \
-        libkrb5-dev \
-        libldap2-dev \
-        libpng-dev \
-        libpq-dev \
-        libxml2-dev \
-        libzip-dev \
-        default-mysql-client \
-        postgresql-client \
-        cron \
-    && apt-get autoremove -y \
-    && docker-php-ext-configure gd --with-freetype --with-jpeg \
-    && docker-php-ext-install -j$(nproc) calendar intl mysqli pdo_mysql gd soap zip \
-    && docker-php-ext-configure pgsql -with-pgsql \
-    && docker-php-ext-install pdo_pgsql pgsql \
-    && docker-php-ext-configure ldap --with-libdir=lib/$(gcc -dumpmachine)/ \
-    && docker-php-ext-install -j$(nproc) ldap \
-    && docker-php-ext-configure imap --with-kerberos --with-imap-ssl \
-    && docker-php-ext-install imap \
-    && mv ${PHP_INI_DIR}/php.ini-production ${PHP_INI_DIR}/php.ini \
-    && rm -rf /var/lib/apt/lists/*
-
-# Get Dolibarr
-RUN curl -fLSs https://github.com/Dolibarr/dolibarr/archive/${DOLIBARR_VERSION}.tar.gz |\
-    tar -C /tmp -xz && \
-    cp -r /tmp/dolibarr-${DOLIBARR_VERSION}/htdocs/* /var/www/html/ && \
-    ln -s /var/www/html /var/www/htdocs && \
-    cp -r /tmp/dolibarr-${DOLIBARR_VERSION}/scripts /var/www/ && \
-    rm -rf /tmp/* && \
-    mkdir -p /var/www/documents && \
-    mkdir -p /var/www/html/custom && \
-    chown -R www-data:www-data /var/www
-
-COPY ./entrypoint.sh /docker-entrypoint.sh
-RUN chmod +x /docker-entrypoint.sh
-
-ENTRYPOINT ["/docker-entrypoint.sh"]
+ENTRYPOINT ["/custom-entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,206 +1,24 @@
 #!/bin/bash
 
-set -eo pipefail
-
-parseDatabaseURI() {
-  eval $(echo "$1" | sed -e "s#^\(\(.*\)://\)\?\(\([^:@]*\)\(:\(.*\)\)\?@\)\?\([^/?]*\)\(/\(.*\)\)\?#${PREFIX:-URI_}SCHEME='\2' ${PREFIX:-URI_}USER='\4' ${PREFIX:-URI_}PASSWORD='\6' ${PREFIX:-URI_}HOSTPORT='\7' ${PREFIX:-URI_}NAME='\9'#")
+# Function to parse a URL into its components
+parse_url() {
+  eval "$(echo "$1" | sed -E \
+    -e "s#^(([^:]+)://)?(([^:@]+)(:([^@]+))?@)?([^/?]+)(/(.*))?#\
+${PREFIX:-URL_}SCHEME='\2' \
+${PREFIX:-URL_}USER='\4' \
+${PREFIX:-URL_}PASSWORD='\6' \
+${PREFIX:-URL_}HOSTPORT='\7' \
+${PREFIX:-URL_}DATABASE='\9'#")"
 }
 
-initDolibarr() {
-  local CURRENT_UID=$(id -u www-data)
-  local CURRENT_GID=$(id -g www-data)
-  usermod -u ${WWW_USER_ID} www-data
-  groupmod -g ${WWW_GROUP_ID} www-data
-
-  if [[ ! -d /var/www/documents ]]; then
-    echo "[INIT] => create volume directory /var/www/documents ..."
-    mkdir -p /var/www/documents
-  fi
-
-  if [[ ! -d /var/www/html/custom ]]; then
-    echo "[INIT] => create volume directory /var/www/html/custom ..."
-    mkdir -p /var/www/html/custom
-  fi
-
-  echo "[INIT] => update PHP Config ..."
-  cat > ${PHP_INI_DIR}/conf.d/dolibarr-php.ini << EOF
-date.timezone = ${PHP_INI_DATE_TIMEZONE}
-sendmail_path = /usr/sbin/sendmail -t -i
-memory_limit = ${PHP_INI_MEMORY_LIMIT}
-upload_max_filesize = ${PHP_INI_UPLOAD_MAX_FILESIZE}
-post_max_size = ${PHP_INI_POST_MAX_SIZE}
-allow_URI_fopen = ${PHP_INI_ALLOW_URI_FOPEN}
-session.use_strict_mode = 1
-disable_functions = pcntl_alarm,pcntl_fork,pcntl_waitpid,pcntl_wait,pcntl_wifexited,pcntl_wifstopped,pcntl_wifsignaled,pcntl_wifcontinued,pcntl_wexitstatus,pcntl_wtermsig,pcntl_wstopsig,pcntl_signal,pcntl_signal_get_handler,pcntl_signal_dispatch,pcntl_get_last_error,pcntl_strerror,pcntl_sigprocmask,pcntl_sigwaitinfo,pcntl_sigtimedwait,pcntl_exec,pcntl_getpriority,pcntl_setpriority,pcntl_async_signals,passthru,shell_exec,system,proc_open,popen
-EOF
-
-  if [[ ! -f /var/www/html/conf/conf.php ]]; then
-    echo "[INIT] => update Dolibarr Config ..."
-    cat > /var/www/html/conf/conf.php << EOF
-<?php
-\$dolibarr_main_url_root='${DOLIBARR_URL_ROOT}';
-\$dolibarr_main_document_root='/var/www/html';
-\$dolibarr_main_URI_root_alt='/custom';
-\$dolibarr_main_document_root_alt='/var/www/html/custom';
-\$dolibarr_main_data_root='/var/www/documents';
-\$dolibarr_main_db_host='${DOLIBARR_DB_HOST}';
-\$dolibarr_main_db_port='${DOLIBARR_DB_PORT}';
-\$dolibarr_main_db_name='${DOLIBARR_DB_NAME}';
-\$dolibarr_main_db_prefix='llx_';
-\$dolibarr_main_db_user='${DOLIBARR_DB_USER}';
-\$dolibarr_main_db_pass='${DOLIBARR_DB_PASSWORD}';
-\$dolibarr_main_db_type='mysqli';
-\$dolibarr_main_authentication='dolibarr';
-\$dolibarr_main_prod=${DOLIBARR_PROD};
-EOF
-    if [[ ! -z ${DOLIBARR_INSTANCE_UNIQUE_ID} ]]; then
-      echo "[INIT] => update Dolibarr Config with instance unique id ..."
-      echo "\$dolibarr_main_instance_unique_id='${DOLIBARR_INSTANCE_UNIQUE_ID}';" >> /var/www/html/conf/conf.php
-    fi
-  fi
-
-  echo "[INIT] => update ownership for file in Dolibarr Config ..."
-  chown www-data:www-data /var/www/html/conf/conf.php
-  chmod 400 /var/www/html/conf/conf.php
-
-  if [[ ${CURRENT_UID} -ne ${WWW_USER_ID} || ${CURRENT_GID} -ne ${WWW_GROUP_ID} ]]; then
-    # Refresh file ownership cause it has changed
-    echo "[INIT] => As UID / GID have changed from default, update ownership for files in /var/ww ..."
-    chown -R www-data:www-data /var/www
-  else
-    # Reducing load on init : change ownership only for volumes declared in docker
-    echo "[INIT] => update ownership for files in /var/www/documents ..."
-    chown -R www-data:www-data /var/www/documents
-  fi
-}
-
-waitForDataBase() {
-  r=1
-
-  while [[ ${r} -ne 0 ]]; do
-    mysql -u ${DOLIBARR_DB_USER} --protocol tcp -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} --connect-timeout=5 -e "status" > /dev/null 2>&1
-    r=$?
-    if [[ ${r} -ne 0 ]]; then
-      echo "Waiting that SQL database is up ..."
-      sleep 2
-    fi
-  done
-}
-
-initializeDatabase() {
-  for fileSQL in /var/www/html/install/mysql/tables/*.sql; do
-    if [[ ${fileSQL} != *.key.sql ]]; then
-      echo "Importing table from `basename ${fileSQL}` ..."
-      sed -i 's/--.*//g;' ${fileSQL} # remove all comment
-      mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} < ${fileSQL}
-    fi
-  done
-
-  for fileSQL in /var/www/html/install/mysql/tables/*.key.sql; do
-    echo "Importing table key from `basename ${fileSQL}` ..."
-    sed -i 's/--.*//g;' ${fileSQL}
-    mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} < ${fileSQL} > /dev/null 2>&1
-  done
-
-  for fileSQL in /var/www/html/install/mysql/functions/*.sql; do
-    echo "Importing `basename ${fileSQL}` ..."
-    sed -i 's/--.*//g;' ${fileSQL}
-    mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} < ${fileSQL} > /dev/null 2>&1
-  done
-
-  for fileSQL in /var/www/html/install/mysql/data/*.sql; do
-    echo "Importing data from `basename ${fileSQL}` ..."
-    sed -i 's/--.*//g;' ${fileSQL}
-    mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} < ${fileSQL} > /dev/null 2>&1
-  done
-
-  echo "Create SuperAdmin account ..."
-  pass_crypted=`echo -n ${DOLIBARR_ADMIN_PASSWORD} | md5sum | awk '{print $1}'`
-  mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} -e "INSERT INTO llx_user (entity, login, pass_crypted, lastname, admin, statut) VALUES (0, '${DOLIBARR_ADMIN_LOGIN}', '${pass_crypted}', 'SuperAdmin', 1, 1);" > /dev/null 2>&1
-
-  echo "Set some default const ..."
-  mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} -e "DELETE FROM llx_const WHERE name='MAIN_VERSION_LAST_INSTALL';" > /dev/null 2>&1
-  mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} -e "DELETE FROM llx_const WHERE name='MAIN_NOT_INSTALLED';" > /dev/null 2>&1
-  mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} -e "DELETE FROM llx_const WHERE name='MAIN_LANG_DEFAULT';" > /dev/null 2>&1
-  mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} -e "INSERT INTO llx_const(name,value,type,visible,note,entity) values('MAIN_VERSION_LAST_INSTALL', '${DOLIBARR_VERSION}', 'chaine', 0, 'Dolibarr version when install', 0);" > /dev/null 2>&1
-  mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} -e "INSERT INTO llx_const(name,value,type,visible,note,entity) VALUES ('MAIN_LANG_DEFAULT', 'auto', 'chaine', 0, 'Default language', 1);" > /dev/null 2>&1
-  mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} -e "INSERT INTO llx_const(name,value,type,visible,note,entity) VALUES ('SYSTEMTOOLS_MYSQLDUMP', '/usr/bin/mysqldump', 'chaine', 0, '', 0);" > /dev/null 2>&1
-
-  # Update ownership after initialisation of modules
-  chown -R www-data:www-data /var/www/documents
-}
-
-migrateDatabase() {
-  TARGET_VERSION="$(echo ${DOLIBARR_VERSION} | cut -d. -f1).$(echo ${DOLIBARR_VERSION} | cut -d. -f2).0"
-  echo "Schema update is required ..."
-  echo "Dumping Database into /var/www/documents/dump.sql ..."
-
-  mysqldump -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} > /var/www/documents/dump.sql
-  r=${?}
-  if [[ ${r} -ne 0 ]]; then
-    echo "Dump failed ... Aborting migration ..."
-    return ${r}
-  fi
-  echo "Dump done ... Starting Migration ..."
-
-  echo "" > /var/www/documents/migration_error.html
-  pushd /var/www/htdocs/install > /dev/null
-  php upgrade.php ${INSTALLED_VERSION} ${TARGET_VERSION} >> /var/www/documents/migration_error.html 2>&1 && \
-  php upgrade2.php ${INSTALLED_VERSION} ${TARGET_VERSION} >> /var/www/documents/migration_error.html 2>&1 && \
-  php step5.php ${INSTALLED_VERSION} ${TARGET_VERSION} >> /var/www/documents/migration_error.html 2>&1
-  r=$?
-  popd > /dev/null
-
-  if [[ ${r} -ne 0 ]]; then
-    echo "Migration failed ... Restoring DB ... check file /var/www/documents/migration_error.html for more info on error ..."
-    mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} < /var/www/documents/dump.sql
-    echo "DB Restored ..."
-    return ${r}
-  else
-    echo "Migration successful ... Enjoy !!"
-  fi
-
-  return 0
-}
-
-lockInstallation() {
-  touch /var/www/documents/install.lock
-  chown www-data:www-data /var/www/documents/install.lock
-  chmod 400 /var/www/documents/install.lock
-}
-
-run() {
-  initDolibarr
-  echo "Current Version is : ${DOLIBARR_VERSION}"
-
-  if [[ ${DOLIBARR_INSTALL_AUTO} -eq 1 && ! -f /var/www/documents/install.lock ]]; then
-    waitForDataBase
-
-    mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} -e "SELECT Q.LAST_INSTALLED_VERSION FROM (SELECT INET_ATON(CONCAT(value, REPEAT('.0', 3 - CHAR_LENGTH(value) + CHAR_LENGTH(REPLACE(value, '.', ''))))) as VERSION_ATON, value as LAST_INSTALLED_VERSION FROM llx_const WHERE name IN ('MAIN_VERSION_LAST_INSTALL', 'MAIN_VERSION_LAST_UPGRADE') and entity=0) Q ORDER BY VERSION_ATON DESC LIMIT 1" > /tmp/lastinstall.result 2>&1
-    r=$?
-    if [[ ${r} -ne 0 ]]; then
-      initializeDatabase
-    else
-      INSTALLED_VERSION=`grep -v LAST_INSTALLED_VERSION /tmp/lastinstall.result`
-      echo "Last installed Version is : ${INSTALLED_VERSION}"
-      if [[ "$(echo ${INSTALLED_VERSION} | cut -d. -f1)" -lt "$(echo ${DOLIBARR_VERSION} | cut -d. -f1)" ]]; then
-        migrateDatabase
-      else
-        echo "Schema update is not required ... Enjoy !!"
-      fi
-    fi
-
-    lockInstallation
-  fi
-}
-
-# prefix variables to avoid conflicts and run parse url function on arg url
-PREFIX="DOLIBARR_DB_" parseDatabaseURI "$DATABASE_URL"
+# Parse the DATABASE_URL and extract components
+PREFIX="DOLIBARR_DB" parse_url "$DATABASE_URL"
 
 # Separate host and port
-DOLIBARR_DB_HOST="$(echo $DOLIBARR_DB_HOSTPORT | sed -e 's,:.*,,g')"
-DOLIBARR_DB_PORT="$(echo $DOLIBARR_DB_HOSTPORT | sed -e 's,^.*:,:,g' -e 's,.*:\([0-9]*\).*,\1,g' -e 's,[^0-9],,g')"
+DOLI_DB_HOST="$(echo $DOLIBARR_DB_HOSTPORT | sed -E 's,:.*,,')"
+DOLI_DB_HOST_PORT="$(echo $DOLIBARR_DB_HOSTPORT | sed -E 's,.*:([0-9]+).*,\1,')"
+DOLI_DB_NAME="$DOLIBARR_DB_DATABASE"
+DOLI_DB_USER="$DOLIBARR_DB_USER"
+DOLI_DB_PASSWORD="$DOLIBARR_DB_PASSWORD"
 
-run
-
-apache2-foreground
+exec /usr/local/bin/docker-run.sh $@
